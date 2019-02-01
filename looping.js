9ec23afd851366794f51471c3d7cbd3f098891c0
Lidya <imlidya18@gmail.com>
2019-02-01 01:38:35 -0500

    Looping

---
+++ looping.js
@@ -0,0 +1,66 @@
+   //1
+
+   var coding = 0;
+   while (coding<20) {
+    coding += 2;
+    console.log(coding + ' - I Love Coding');
+   }
+   var developer = 22;
+   while(developer<2){
+     coding -= 2;
+     console.log(developer + ' I will become fullstack developer');
+   }
+
+   //2
+
+    var coding;
+    for (coding = 0; coding=<20; coding ++ ){
+        console.log(coding + ' - I Love Coding');
+    }
+    var developer;
+    for (developer = 22; developer>=1; coding -- ){
+        console.log(coding + ' - I will become fullstack developer');
+    }
+   
+    //3
+
+    for(var angka = 1; angka <= 100; angka++){
+      if((angka%2)==1){
+        document.write('gajil' + '<br>');
+      }
+      else if ((angka%2)===0) {
+        document.write('genap' + '<br>');
+      }
+    }
+    
+    
+    for(var tambah2 = 1; tambah2 <= 100; tambah2+=2){
+      if((tambah2%3)===0){
+        document.write(tambah2 + ' KELIPATAN 3' +'<br>');
+      }
+      else {
+        document.write(' ' + '<br>');
+      }
+    }
+    
+    
+    for(var tambah5 = 1; tambah5 <= 100; tambah5+=5){
+      if((tambah5%6)===0){
+        document.write(tambah5 + ' KELIPATAN 6' +'<br>');
+      }
+      else {
+        document.write(' ' + '<br>');
+      }
+    }
+    
+    
+    for(var tambah9 = 1; tambah9 <= 100; tambah9+=9){
+      if((tambah9%10)===0){
+        document.write(tambah9 + ' KELIPATAN 10' +'<br>');
+      }
+      else {
+        document.write('' + '<br>');
+      }
+    }
+
+     
