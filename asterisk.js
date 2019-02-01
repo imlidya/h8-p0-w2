0934dd26f68736af6e61f4752b744005f7f343d7
Lidya <imlidya18@gmail.com>
2019-02-01 02:46:37 -0500

    Tugas Asterisk

---
+++ asterisk.js
@@ -0,0 +1,32 @@
+
+
+//1 
+
+var rows1 = 5;
+while (rows1 >= 1){
+    console.log("*");
+    rows1--;
+}
+
+
+//2
+
+var rows2 = 5;
+var bintang = "";
+for (var i = rows2; i >= 1; i--){
+    while (rows2 >= 1){
+        bintang = bintang+"*";
+        rows2--;
+    }
+    console.log(bintang);
+}
+
+
+//3
+var rows3 = 5;
+var bintang = "";
+for (var i = rows3; i >= 1; i--){
+    bintang = bintang+"*";
+    console.log(bintang);
+}
+
