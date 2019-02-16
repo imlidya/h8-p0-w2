68ebd64801ca78679bb68d57abfb394dcf36317e
imlidya <46871323+imlidya@users.noreply.github.com>
2019-02-16 04:28:52 -0500

    Update deepsumweek4.js

--- deepsumweek4.js
+++ deepsumweek4.js
@@ -2,7 +2,6 @@ function deepSum(arr) {
 
  var counter= 0
  for(var i=0; i<arr.length; i++){
-  //  console.log(arr[i])
    for(var j=0; j<arr[i].length; j++){
     for(var k=0; k<arr[i][j].length; k++){
       counter += arr[i][j][k]
