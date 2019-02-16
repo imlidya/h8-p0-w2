58aa2fdc57b7504c2dfece19adacdd498dbb92f9
imlidya <46871323+imlidya@users.noreply.github.com>
2019-02-16 05:09:49 -0500

    Update and rename TugasWeek2 to tokoxweek4.js

---
+++ tokoxweek4.js
@@ -0,0 +1,61 @@
+function countProfit(shoppers) {
+  var listBarang = [
+    ['Sepatu Stacattu', 1500000, 10],
+    ['Baju Zoro', 500000, 2],
+    ['Sweater Uniklooh', 175000, 1],
+  ];
+  
+  if(shoppers.length===0){
+    return []
+  }
+  
+  var result = [];
+  for (var i = 0; i < listBarang.length; i++) {
+    var pembeli = [];
+    var sisa = 0;
+    for (var j = 0; j < shoppers.length; j++) {
+      if (
+        shoppers[j].product === listBarang[i][0] &&
+        sisa + shoppers[j].amount <= listBarang[i][2]
+      ) {
+        sisa += shoppers[j].amount;
+        pembeli.push(shoppers[j].name);
+      }
+    }
+    var obj = {
+      products : listBarang[i][0],
+      shoppers : pembeli,
+      leftOver : listBarang[i][2] - sisa,
+      totalProfit : listBarang[i][1] * sisa
+    }
+    result.push(obj);
+  }
+  return result;
+}
+
+// TEST CASES
+console.log(countProfit([{
+    name: 'Windi',
+    product: 'Sepatu Stacattu',
+    amount: 2
+}, {
+    name: 'Vanessa',
+    product: 'Sepatu Stacattu',
+    amount: 3
+}, {
+    name: 'Rani',
+    product: 'Sweater Uniklooh',
+    amount: 2
+}]));
+//[ { product: 'Sepatu Stacattu',
+//   shoppers: [ 'Windi', 'Vanessa' ],
+//   leftOver: 5,
+//   totalProfit: 7500000 },
+// { product: 'Baju Zoro',
+//   shoppers: [],
+//   leftOver: 2,
+//   totalProfit: 0 },
+// { product: 'Sweater Uniklooh',
+//   shoppers: [],
+//   leftOver: 1,
+//   totalProfit: 0 } ]
