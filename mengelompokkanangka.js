93777ed42b6016d34a360712df525d9acf4d3026
imlidya <46871323+imlidya@users.noreply.github.com>
2019-05-05 11:22:26 -0400

    Update mengelompokkanangka.js

--- mengelompokkanangka.js
+++ mengelompokkanangka.js
@@ -1,20 +1,23 @@
 function mengelompokkanAngka(arr) {
-  var counter = [[],[],[]];
-  for(var i = 0; i < arr.length; i++){
-    if(arr[i] % 3 === 0){
-      counter[2].push(arr[i]);
-    } 
-    else if(arr[i] % 2 === 0){
-      counter[0].push(arr[i]);
-    } 
-    else {
-      counter[1].push(arr[i]);
+    var result = [[],[],[]];
+    for(var i = 0; i < arr.length; i++){
+      if(arr[i] % 3 === 0){
+        result[2].push(arr[i]);
+      } 
+      else if(arr[i] % 2 === 0){
+        result[0].push(arr[i]);
+      } 
+      else if(arr[i] % 2 === 1){
+        result[1].push(arr[i]);
+      }
+      
     }
-    
+    return result;
   }
-  return counter;
-}
-console.log(mengelompokkanAngka([2, 4, 6])); 
-console.log(mengelompokkanAngka([1, 2, 3, 4, 5, 6, 7, 8, 9]));
-console.log(mengelompokkanAngka([100, 151, 122, 99, 111])); 
-console.log(mengelompokkanAngka([])); 
+
+
+// TEST CASES
+console.log(mengelompokkanAngka([2, 4, 6])); // [ [2, 4], [], [6] ]
+console.log(mengelompokkanAngka([1, 2, 3, 4, 5, 6, 7, 8, 9])); // [ [ 2, 4, 8 ], [ 1, 5, 7 ], [ 3, 6, 9 ] ]
+console.log(mengelompokkanAngka([100, 151, 122, 99, 111])); // [ [ 100, 122 ], [ 151 ], [ 99, 111 ] ]
+console.log(mengelompokkanAngka([])); // [ [], [], [] ]
