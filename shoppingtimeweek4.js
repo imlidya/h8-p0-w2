d147400eb109982b4771e91409079702a8089e6d
imlidya <46871323+imlidya@users.noreply.github.com>
2019-05-07 02:32:26 -0400

    Update shoppingtimeweek4.js

--- shoppingtimeweek4.js
+++ shoppingtimeweek4.js
@@ -1,38 +1,36 @@
 function shoppingTime(memberId, money) {
- 
- var listPurchased2 =[]
- var listPurchased = [
-   ['Sepatu Stacattu', 1500000],
-   ['Baju Zoro', 500000],
-   ['Baju H&N', 250000],
-   ['Sweater Uniklooh', 175000],
-   ['Casing Handphone', 50000]
- ]
-  var change =money;
+  // you can only write your code here!
+  var change=money
+  var result =[]
+  var listPurchased = [
+    ['Sepatu Stacattu', 1500000],
+    ['Baju Zoro', 500000],
+    ['Baju H&N', 250000],
+    ['Sweater Uniklooh', 175000],
+    ['Casing Handphone', 50000]
+  ]
+
   if(memberId === undefined || memberId === ''){
-   return 'Mohon maaf, toko X hanya berlaku untuk member saja'
-  } 
-  if(money<50000){
-    return 'Mohon maaf, uang tidak cukup'
+      return 'Mohon maaf, toko X hanya berlaku untuk member saja'
+  } else if(money < 50000){
+      return 'Mohon maaf, uang tidak cukup'
   }
-  for(i=0; i<listPurchased.length; i++){
-    // console.log(listPurchased[i])
-    // var listPurchased2 =[]
+  for(var i=0; i<listPurchased.length; i++){
     if(change >= listPurchased[i][1]){
-      change -=listPurchased[i][1]; 
-      listPurchased2.push(listPurchased[i][0])  
+        change -= listPurchased[i][1]
+        result.push(listPurchased[i][0])
     }
-  } 
-  var obj = {
-   memberId: memberId,
-   money: money,
-   listPurchased: listPurchased2,
-   changeMoney: change
   }
-  
- return obj
+  var obj = {
+    memberId: memberId,
+    money: money,
+    listPurchased: result,
+    changeMoney: change
+   }
+   
+  return obj
 }
- 
+
 // TEST CASES
 console.log(shoppingTime('1820RzKrnWn08', 2475000));
   //{ memberId: '1820RzKrnWn08',
@@ -45,11 +43,11 @@ console.log(shoppingTime('1820RzKrnWn08', 2475000));
   //    'Casing Handphone' ],
   // changeMoney: 0 }
 console.log(shoppingTime('82Ku8Ma742', 170000));
-// //{ memberId: '82Ku8Ma742',
-// // money: 170000,
-// // listPurchased:
-// //  [ 'Casing Handphone' ],
-// // changeMoney: 120000 }
+//{ memberId: '82Ku8Ma742',
+// money: 170000,
+// listPurchased:
+//  [ 'Casing Handphone' ],
+// changeMoney: 120000 }
 console.log(shoppingTime('', 2475000)); //Mohon maaf, toko X hanya berlaku untuk member saja
 console.log(shoppingTime('234JdhweRxa53', 15000)); //Mohon maaf, uang tidak cukup
 console.log(shoppingTime()); ////Mohon maaf, toko X hanya berlaku untuk member saja
