5065c26da16c3855556e8e571e4064b5336f4edb
imlidya <46871323+imlidya@users.noreply.github.com>
2019-05-07 02:36:15 -0400

    Update naikangkotweek4.js

--- naikangkotweek4.js
+++ naikangkotweek4.js
@@ -1,34 +1,33 @@
 function naikAngkot(arrPenumpang) {
-    rute = ['A', 'B', 'C', 'D', 'E', 'F'];
-    //your code here
+  rute = ['A', 'B', 'C', 'D', 'E', 'F'];
+  var result=[];
 
-    var tmp = [];
-
-    for(var i = 0; i < arrPenumpang.length; i++) {
-      
-      for(var j = 0; j < rute.length; j++) {
-        if(arrPenumpang[i][1] === rute[j]) {
-          var titik1 = j;
-        }else if(arrPenumpang[i][2] === rute[j]) {
-          var titik2 = j;
-        }
-      }
-
-      var obj = {};
-      obj.penumpang = arrPenumpang[i][0];
-      obj.naikDari = arrPenumpang[i][1];
-      obj.tujuan = arrPenumpang[i][2];
-      obj.bayar = (titik2 - titik1) * 2000;
-
-      tmp.push(obj);
-
-    }
-
-    return tmp;
-
+  if(!arrPenumpang.length){
+    return []
   }
-   console.log(naikAngkot([['Dimitri', 'B', 'F'], ['Icha', 'A', 'B']]));
-  // [ { penumpang: 'Dimitri', naikDari: 'B', tujuan: 'F', bayar: 8000 },
-  //   { penumpang: 'Icha', naikDari: 'A', tujuan: 'B', bayar: 2000 } ]
-  
-  console.log(naikAngkot([])); //[]
+  for(var i=0; i<arrPenumpang.length; i++){
+    for(var j=0; j<rute.length; j++){
+      if(arrPenumpang[i][1]===rute[j]){
+       var mulai= rute[j]
+      } 
+      if(arrPenumpang[i][2]===rute[j]){
+      var akhir= rute[j]
+      }
+    }
+    var jarak= +1;
+    var harga = 2000*jarak;
+    var obj ={
+      penumpang: arrPenumpang[i][0],
+      naikDari: mulai,
+      tujuan: akhir,
+      bayar: harga
+    }
+    result.push(obj)
+  }
+  return result
+}
+console.log(naikAngkot([['Dimitri', 'B', 'F'], ['Icha', 'A', 'B']]));
+// [ { penumpang: 'Dimitri', naikDari: 'B', tujuan: 'F', bayar: 8000 },
+//   { penumpang: 'Icha', naikDari: 'A', tujuan: 'B', bayar: 2000 } ]
+
+console.log(naikAngkot([])); //[]
