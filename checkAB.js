c9a8691cd201691650ab5c242a1bdc089b02c957
imlidya <46871323+imlidya@users.noreply.github.com>
2019-05-07 02:26:21 -0400

    Update checkAB.js

--- checkAB.js
+++ checkAB.js
@@ -1,35 +1,26 @@
-function checkAB(str){
-
-var a=[];
-var b=[];
-
-  for(var i=0; i<str.length; i++){
-    if(str[i]==='a'){
-      a.push(i);
-    } if(str[i]==='b'){
-      b.push(i);
-    }
-  }
-  if(a.length === 0 || b.length === 0){
-    return false;
-  }
-
-  for(var i=0; i<a.length; i++){
-    for(var j=0; j<b.length; j++){
-      
-      var jumlah= (a[i] - b[j]) - 1;
-        if (jumlah<0){
-          jumlah= (b[j] - a[i]) -1;
+function checkAB(num) {
+    var tempA=[]
+    var tempB=[]
+    for(var i=0; i<num.length; i++){
+        if(num[i] === 'a'){
+            tempA.push(i)
+            // console.log(tempA)
+        } else if(num[i] === 'b'){
+            tempB.push(i)
         }
-        if(jumlah === 3){
-            return true;
-        } 
-      }
     }
-    return false;
-}
-console.log(checkAB('lane borrowed')); // true
-console.log(checkAB('i am sick')); // false
-console.log(checkAB('you are boring')); // true
-console.log(checkAB('barbarian')); // true
-console.log(checkAB('bacon and meat')); // false
+    for(var i=0; i<tempA.length; i++){
+        for(var j=0; j<tempB.length; j++){
+            var jumlah = (tempA[i]-tempB[j])-1
+            if (jumlah<0){
+                jumlah= (tempB[j] - tempA[i]) -1;
+            }
+            if(jumlah === 3){
+                return true
+            }
+        }
+    }
+    return false
+
+  }
+  
