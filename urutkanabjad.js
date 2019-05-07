2f54f589e029d35450ed22018d5f660f58fe0305
imlidya <46871323+imlidya@users.noreply.github.com>
2019-05-07 02:22:01 -0400

    Update urutkanabjad.js

--- urutkanabjad.js
+++ urutkanabjad.js
@@ -1,18 +1,20 @@
 function urutkanAbjad(str) {
-
-var alph ="abcdefghjklmnopqrstuvwxyz";
-var result = '';
-
-  for(var i = 0; i < alph.length; i++) {
-    for(var j = 0; j < str.length; j++) {
-      if(alph[i] === str[j]) {
-        result = result + str[j];
+  // you can only write your code here!
+  var alph ="abcdefghjklmnopqrstuvwxyz";
+  var result = '';
+  
+    for(var i = 0; i < alph.length; i++) {
+      for(var j = 0; j < str.length; j++) {
+        if(alph[i] === str[j]) {
+          result += str[j];
+        }
       }
     }
-  }
-
-  return result;
+  
+    return result;
 }
+
+// TEST CASES
 console.log(urutkanAbjad('hello')); // 'ehllo'
 console.log(urutkanAbjad('truncate')); // 'acenrttu'
 console.log(urutkanAbjad('developer')); // 'deeeloprv'
