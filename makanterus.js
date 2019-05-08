94097f212c0a9f63b620c9487c2ce8b6a3e8d76c
imlidya <46871323+imlidya@users.noreply.github.com>
2019-05-08 01:47:15 -0400

    Update makanterus.js

--- makanterus.js
+++ makanterus.js
@@ -1,19 +1,20 @@
 function makanTerusRekursif(waktu) {
-  var makan = 0;
-  var perubahanWaktu;
-  if (waktu === 0) {
-    return 0;
-  } else if (waktu < 15) {
-    return 1;
-  } else {
-    makan++;
-    perubahanWaktu = waktu - 15;
+    // you can only write your code here!
+    if(waktu === 0){
+        return 0
+    } else if(waktu<15){
+        return 1
+    } else {
+        var perubahanWaktu = waktu-15
+        var makan = 1
+    }
+    
+    return makan + makanTerusRekursif(perubahanWaktu)
   }
-  return makan + makanTerusRekursif(perubahanWaktu);
-}
-
-console.log(makanTerusRekursif(66)); // 5
-console.log(makanTerusRekursif(100)); // 7
-console.log(makanTerusRekursif(90)); // 6
-console.log(makanTerusRekursif(10)); // 1
-console.log(makanTerusRekursif(0)); // 0
+  
+  // TEST CASES
+  console.log(makanTerusRekursif(66)); // 5
+  console.log(makanTerusRekursif(100)); // 7
+  console.log(makanTerusRekursif(90)); // 6
+  console.log(makanTerusRekursif(10)); // 1
+  console.log(makanTerusRekursif(0)); // 0
