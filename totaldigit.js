4a215ad579afbee58f5ced12b2bfbb7cc3aa2a32
imlidya <46871323+imlidya@users.noreply.github.com>
2019-05-08 01:49:01 -0400

    Update totaldigit.js

--- totaldigit.js
+++ totaldigit.js
@@ -1,30 +1,17 @@
 function totalDigitRekursif(angka) {
-  var angkaString=angka.toString();
-  if(angkaString.length===0){
-      return 0;
-  }
-  else{
-      var angkaAwal= parseInt(angkaString[0])
-      var angkaNext=angkaString.substring(1);
-      return angkaAwal + totalDigitRekursif(angkaNext)
+  // you can only write your code here!
+  var angkaStr=angka.toString()
+  var angkaArr=angkaStr.split("")
+  angkaArr.shift()
+  var angka2=Number(angkaArr.join(""))
+  var angkaawal = parseInt(angkaStr[0])
+  if(angka === 0){
+      return 0
+  } else {
+      return angkaawal + totalDigitRekursif(angka2)
   }
 }
-//2
-function totalDigitRekursif(angka) {
-   // you can only write your code here!
-   var angkaStr=angka.toString()
-   var angkaArr=angkaStr.split("")
-   result=Number(angkaArr[0])
-   angkaArr.shift()
-   angka2=Number(angkaArr.join(""))
-   if(angkaArr.length===0){
-       return result
-   }
-   else {
-       return result+totalDigitRekursif(angka2)
-   }
 
- }
 // TEST CASES
 console.log(totalDigitRekursif(512)); // 8
 console.log(totalDigitRekursif(1542)); // 12
