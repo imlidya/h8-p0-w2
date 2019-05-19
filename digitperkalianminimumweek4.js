4d6ed18c339f1b2329ec8db81ee95cf9319ade2d
imlidya <46871323+imlidya@users.noreply.github.com>
2019-05-19 11:09:13 -0400

    Update digitperkalianminimumweek4.js

--- digitperkalianminimumweek4.js
+++ digitperkalianminimumweek4.js
@@ -29,32 +29,3 @@ console.log(digitPerkalianMinimum(179)); // 4
 console.log(digitPerkalianMinimum(1)); // 2
 
 
-//cara2tapicumanbisatestcase1-3why:(
-
-function digitPerkalianMinimum(angka){
-
-
-var result=[];
-
-  for(var i=0; i<angka; i++){
-    for(var j=0; j<angka; j++){
-        result.push(join);
-      }
-      if(i*j === angka){
-        var join=i.toString() + j.toString();
-    }
-  }
-  var minTemp = result[0].length;
-   for(var k=0; k<result[k].length; k++){
-     if(result[k].length < minTemp){
-       minTemp= result[k].length;
-     }
-   }
-   return minTemp;
-}
-
-console.log(digitPerkalianMinimum(24)); // 2
-console.log(digitPerkalianMinimum(90)); // 3
-console.log(digitPerkalianMinimum(20)); // 2
-console.log(digitPerkalianMinimum(179)); // 4
-console.log(digitPerkalianMinimum(1)); // 2
